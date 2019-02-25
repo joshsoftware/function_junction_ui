a27f7d84a6a0ba57a3f34e3276a4657d1cc2e427
Suhas More <suhas.more@joshsoftware.com>
2019-02-25 18:48:07 +0530

    Ejected react script and added scss loaders

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import AppLayout from '../layout/Layout';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import routes from '../../config/routes';
 import 'antd/dist/antd.css';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
