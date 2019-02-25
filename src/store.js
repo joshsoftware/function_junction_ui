7b8f8a7946b01af7ed5a73a2c3f4e508b18f8ff2
Suhas More <suhas.more@joshsoftware.com>
2019-02-25 18:02:45 +0530

    aaded redux dev tool

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers/'
 import sagas from './sagas/'
 
 const sagaMiddleWare = createSagaMiddleware();
-
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    applyMiddleware(sagaMiddleWare)
+    composeEnhancer(applyMiddleware(sagaMiddleWare))
 );
 sagaMiddleWare.run(sagas);
 
