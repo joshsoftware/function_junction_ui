4b1d694a20d26a9691d940b45b5003c5c6f0bc93
Suhas More <suhas.more@joshsoftware.com>
2019-02-26 10:50:57 +0530

    merged conflicts

---
+++ src/components/app/Carousel/index.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import { Carousel } from 'antd';
+import './Carousel.scss';
+
+const getItems = (items) => {
+    return items.map(item => <div>{item}</div>)
+}
+const Slider = ({items}) => <Carousel dots autoplay effect="fade">
+    {getItems(items || [1,2,3,4,5])}
+</Carousel>
+
+export default Slider;
--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import routes from '../../config/routes';
 import asyncComponent from '../../HOC/AsyncComponent';
+import Slider from '../app/Carousel';
 import CreateEventContainer from '../../containers/createEventContainer';
 
 const BrowseEvents = asyncComponent(() => import('../../containers/BrowseEvents'), 'BrowseEvents')
 
+const items=[
+    <img src="/assets/img/1.jpg" alt="1"/>,
+    <img src="/assets/img/2.jpg" alt="2"/>,
+    <img src="/assets/img/3.jpg" alt="3"/>,
+    <img src="/assets/img/4.jpg" alt="4"/>,
+ ]
+
+const AllEvents = () => <Slider items={items}/>;
 const upcomingEvents =() => <h1>Upcomming Events</h1>;
 const myEvents =() => <h1>My Events</h1>;
 
@@ -14,7 +23,7 @@ class MainContent extends Component {
         <div>
             <Switch>
                 <Route exact path={routes.allEvents} component={BrowseEvents}/>
-                <Route exact path={routes.upcomingEvents} component={upcomingEvents}/>
+                <Route exact path={routes.upcomingEvents} component={AllEvents}/>
                 <Route exact path={routes.myEvents} component={myEvents}/>
                 <Route exact path={routes.createEvent} component={CreateEventContainer}/>
             </Switch>
