ec6cd5f1f7cf61b659aaaad35982c0adbbc9b628
Suhas More <suhas.more@joshsoftware.com>
2019-03-10 13:57:30 +0530

    removed unnecessary prop form Card

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -11,7 +11,6 @@ const eventCard = ({id, title, desc, startDateTime, history, ...rest}) => {
     cover={<img alt="example" src="https://image.shutterstock.com/image-vector/vector-hand-drawn-acrylic-stroke-450w-450333322.jpg" />}
     // actions={[<Tooltip placement='top' title='Invite' ><Icon type="user" /></Tooltip>, <Tooltip placement='top' title='Edit'><Icon type="edit" /></Tooltip>]}
     onClick={() => history.push(`/event-details/${id}`)}
-    {...rest}
   >
     <Meta
       avatar={<div><div>{moment(startDateTime).format('MMM DD YY')}</div><div>{moment(startDateTime).format('h:m:a')}</div></div>}
