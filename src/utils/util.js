dd8dafdbeb863a693502758dd0e010794d7771e4
Ajit <ajit.fawade@joshsoftware.com>
2019-03-12 14:56:23 +0530

    Added random color generator util.

--- src/utils/util.js
+++ src/utils/util.js
@@ -10,3 +10,12 @@ export function validateURL(url) {
   }
   return true;
 }
+
+export function generateRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
