44ddabcc2e201b53d66608509eabb5b055ff70d2
Suhas More <suhas.more@joshsoftware.com>
2019-03-13 16:05:20 +0530

    Added online/offline status

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Alert } from 'antd';
 import AppLayout from '../layout/Layout';
 import routes from 'UTILS/routes';
 import 'antd/dist/antd.css';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Route path={routes.root} component={AppLayout} />
-        </Router>
-      </div>
-    );
-  }
-}
 
+
+const App = () => {
+
+  const [online, changeStatus] = useState(true);
+    
+  function changeStatus(value) {
+    online = value;
+  }
+
+  useEffect(()=> {
+    window.addEventListener('online', () => {
+      changeStatus(navigator.onLine);
+    });
+    window.addEventListener('offline', () => {
+      changeStatus(navigator.onLine);
+    });
+  });
+  return (
+    <div className="App">
+    <div className="online">
+      {!online && <Alert message="Seems like you lost internet connection." type="error" showIcon closable />}
+    </div>
+      <Router>
+        <Route path={routes.root} component={AppLayout} />
+      </Router>
+    </div>
+  );
+}  
 export default App;
