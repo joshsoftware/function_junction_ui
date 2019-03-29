7e1495c2a133e83d3c0bdd20ccf3130d6f710043
Suhas More <suhas.more@joshsoftware.com>
2019-03-29 11:40:31 +0530

    Removed unused code

--- src/components/BrowseEvents/index.js
+++ src/components/BrowseEvents/index.js
@@ -9,21 +9,14 @@ import { getUser } from '../../actions/userDetailsAction';
 import './BrowseEvents.scss';
 
 const items=[
-    <img src="https://cdn.evbstatic.com/s3-build/perm_001//8d8a56/django/images/homepage/bg-desktop-snowglobe.jpg" alt="1" width="100%"/>,
+    <img src={`${window.origin}/assets/img/5.png`} alt="1" width="100%"/>,
+    <img src={`${window.origin}/assets/img/6.png`} alt="1" width="100%"/>,
     <img src="https://cdn.evbstatic.com/s3-build/perm_001//8b6c63/django/images/homepage/bg-desktop-generationdiy.jpg" alt="2" width="100%"/>,
     <img src="https://cdn.evbstatic.com/s3-build/perm_001//054546/django/images/homepage/bg-desktop-wanderlust.jpg" alt="3" width="100%"/>,
     <img src="https://cdn.evbstatic.com/s3-build/perm_001//b41172/django/images/homepage/bg-desktop-rapoport.jpg" alt="4" width="100%"/>,
  ]
 
 class BrowseEvents extends PureComponent {
-    constructor(props) {
-        super(props);
-        const { cookies } = props;
-        this.state = {
-            name: cookies.get('_intranet_session')
-        }
-    }
-
     componentDidMount () {
         getUser()
         .then(data => {
